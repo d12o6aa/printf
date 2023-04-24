ff4ee30acd41c86c32ed07b0f43948700062f8f0
d12o6aa <doaakarem798@gmail.com>
2023-04-24 11:51:35 -0700

    2

--- _printf.c
+++ _printf.c
@@ -6,37 +6,24 @@
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
- *
  * Return: the length of the string.
  */
 int _printf(const char *format, ...)
 {
-    va_list list ;
-    
-    int i=0, j, m=0;
-    
-    format form[] = {
-        {"%s", printf_string},
-        {"%c", printf_char},
-        {"%%", printf_37},
-        {"%i", printf_int},
-        {"%R", printf_rot13},
-        {"%S", printf_exclusive_string},
-        {"%o", printf_oct},
-        {"%p", printf_pointer},
-        {"%X", printf_HEX},
-        {"%u", printf_unsigned},
-        {"%r", printf_srev},
-        {"%d", printf_dec},
-        {"%b", printf_bin},
-        {"%x", printf_hex}
-        };
-        
+    va_list list;
+    int i = 0, j, m = 0;
+	format form[] = {
+    {"%s", printf_string}, {"%c", printf_char},
+    {"%%", printf_37},
+    {"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+    {"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+    {"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+    {"%S", printf_exclusive_string}, {"%p", printf_pointer}
+    };
     va_start(list, format);
-        
     if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-		
+
 Here:
 	while (format[i] != '\0')
 	{
@@ -58,3 +45,4 @@ Here:
 	va_end(list);
 	return (m);
 }
+
